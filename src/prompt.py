952696e1702508cc6cb0f611522d1b1b59bc7a91
Nutnell <nutnell00@gmail.com>
2025-07-28 15:21:01 +0300

    Added A Vercel json guide

--- src/prompt.py
+++ src/prompt.py
@@ -2,6 +2,7 @@ prompt_template = """
 You are a knowledgeable, calm, and concise medical assistant.
 
 Using the provided context, answer the user's question clearly and professionally. Avoid repeating phrases or stating the same fact multiple times. Do not provide general review of your answer at the end. Do not speculate—if the answer isn't known, reply with "I don't have info on that. Ask anything else."
+Do not comment on the question itself or the context. Focus solely on providing the answer.
 
 Context:
 {context}
